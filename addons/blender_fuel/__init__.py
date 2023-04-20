1f53d230ba31f62d6c8d282012a3f7ef0f362411
widberg <jaredwidberg@gmail.com>
2023-04-20 17:14:02 -0400

    Begin export operation

--- addons/blender_fuel/__init__.py
+++ addons/blender_fuel/__init__.py
@@ -8,7 +8,7 @@ from . import mesh_z
 
 class ImportFUELMeshZ(Operator, ImportHelper):
     """Import FUEL Mesh_Z"""
-    bl_idname = "blender_fuel.mesh_z"
+    bl_idname = "blender_fuel.import_mesh_z"
     bl_label = "Import FUEL Mesh_Z"
 
     filename_ext = ".Mesh_Z.d"
@@ -32,14 +32,59 @@ def menu_func_import_fuel_mesh_z(self, context):
                          text="FUEL Mesh_Z (.Mesh_Z.d)")
 
 
-def register():
+class ExportFUELMeshZ(Operator, ImportHelper):
+    """Export FUEL Mesh_Z"""
+    bl_idname = "blender_fuel.export_mesh_z"
+    bl_label = "Export FUEL Mesh_Z"
+
+    filename_ext = ".Mesh_Z.d"
+
+    filter_glob: StringProperty(
+        default="*.Mesh_Z.d",
+        options={'HIDDEN'},
+        subtype='DIR_PATH',
+        maxlen=2048,
+    )
+
+    def execute(self, context):
+        mesh_z.export_to_object_directory_path(self.filepath)
+
+        return {'FINISHED'}
+
+
+def menu_func_export_fuel_mesh_z(self, context):
+    self.layout.operator(ExportFUELMeshZ.bl_idname,
+                         text="FUEL Mesh_Z (.Mesh_Z.d)")
+
+
+def register_import():
     bpy.utils.register_class(ImportFUELMeshZ)
     bpy.types.TOPBAR_MT_file_import.append(menu_func_import_fuel_mesh_z)
 
 
-def unregister():
+def register_export():
+    bpy.utils.register_class(ExportFUELMeshZ)
+    bpy.types.TOPBAR_MT_file_export.append(menu_func_export_fuel_mesh_z)
+
+
+def register():
+    register_import()
+    register_export()
+
+
+def unregister_import():
     bpy.utils.unregister_class(ImportFUELMeshZ)
     bpy.types.TOPBAR_MT_file_import.remove(menu_func_import_fuel_mesh_z)
+
+
+def unregister_export():
+    bpy.utils.unregister_class(ExportFUELMeshZ)
+    bpy.types.TOPBAR_MT_file_export.remove(menu_func_export_fuel_mesh_z)
+
+
+def unregister():
+    unregister_import()
+    unregister_export()
 
 
 bl_info = {
--- addons/blender_fuel/mesh_z.py
+++ addons/blender_fuel/mesh_z.py
@@ -1,13 +1,14 @@
 import bpy
 import os
 import json
+from itertools import chain
 
 def load_mesh_buffers(object_json):
     obs = []
     for i in range(0, len(object_json['body']['mesh_buffers']['index_buffers'])):
         me = bpy.data.meshes.new("Mesh" + str(i))
 
-        positions = [x['position'] for x in object_json['body']['mesh_buffers']['vertex_buffers'][i]['vertices']]
+        positions = [[x['position'][0], x['position'][2], x['position'][1]] for x in object_json['body']['mesh_buffers']['vertex_buffers'][i]['vertices']]
 
         indices = object_json['body']['mesh_buffers']['index_buffers'][i]['indices']
         faces = [indices[i:i + 3] for i in range(0, len(indices), 3)]
@@ -19,7 +20,7 @@ def load_mesh_buffers(object_json):
             for vert_idx, loop_idx in zip(face.vertices, face.loop_indices):
                 uv_layer.data[loop_idx].uv = object_json['body']['mesh_buffers']['vertex_buffers'][i]['vertices'][vert_idx]['uv']
 
-        normals = [x['normal'] for x in object_json['body']['mesh_buffers']['vertex_buffers'][i]['vertices']]
+        normals = [[x['normal'][0], x['normal'][2], x['normal'][1]] for x in object_json['body']['mesh_buffers']['vertex_buffers'][i]['vertices']]
         me.normals_split_custom_set_from_vertices(normals)
         
         me.calc_tangents()
@@ -41,3 +42,26 @@ def import_from_object_directory_path(directory_path):
         collection.objects.link(mesh_bufffer_object)
 
     return collection
+
+def export_to_object_directory_path(directory_path):
+    object_json_path = os.path.join(directory_path, 'object.json')
+    ob = bpy.context.object
+    me = ob.data
+
+    vertices = [{'position': [(ob.matrix_world @ v.co)[0], (ob.matrix_world @ v.co)[2], (ob.matrix_world @ v.co)[1]], 'tangent': [0, 0, 0], 'pad0': 1, 'normal': [v.normal[0], v.normal[2], v.normal[1]], 'pad1': 255, 'uv': [0, 0], 'luv': [0, 0]} for v in me.vertices]
+    for face in me.polygons:
+        for vert_idx, loop_idx in zip(face.vertices, face.loop_indices):
+            uv_coords = me.uv_layers.active.data[loop_idx].uv
+            vertices[vert_idx]['uv'] = [1 - uv_coords.x, 1 - uv_coords.y]
+            tangent = me.loops[loop_idx].tangent
+            vertices[vert_idx]['tangent'] = [tangent[0], tangent[2], tangent[1]]
+
+    indices = list(chain.from_iterable([(l.vertices[0], l.vertices[1], l.vertices[2]) for l in me.loop_triangles]))
+
+    object_json = {
+        'vertices': vertices,
+        'indices': indices,
+    }
+
+    with open(object_json_path, 'w') as object_json_file:
+        json.dump(object_json, object_json_file, indent=2)
